1ccf42b288099966678c9e36fc44af4de934687e
Yuri Shadrin <ymshadrin@gmail.com>
2023-08-20 14:32:30 +0300

    Some improvements

--- config-server/src/main/java/dev/example/configserver/ConfigServer.java
+++ config-server/src/main/java/dev/example/configserver/ConfigServer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigServer {
     public static void main(String[] args) {
